23a56e8b893a21b52fb11cfadb06b74aad49c51f
Mosesmatisi <mosesmatisi@gmail.com>
2024-11-05 21:55:23 +0300

    Add files via upload

---
+++ C PROGRAM TO CHECK IF A NUMBER IS EVEN OR ODD.c
@@ -0,0 +1,16 @@
+//program to check whether a number is Even or Odd
+#include <stdio.h>
+int main(){
+	int num;
+	printf("Enter an integer: ");
+	scanf("%d", &num);
+	
+	if(num %2==0){
+		printf("%d is even.", num);
+	}
+	else{
+		printf("%d is odd.", num);
+	}
+	
+	return 0;
+}
