be928b5c70df5bc7f30509a814dabfde32c7a076
Mosesmatisi <mosesmatisi@gmail.com>
2024-11-09 18:25:50 +0300

    Add files via upload

---
+++ Program to read name and marks of students.c
@@ -0,0 +1,32 @@
+// C program that reads the names and marks of students
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *fptr;
+    char name[70];
+    int marks, i, n;
+
+    fptr = fopen("C:\\Users\\Administrator\\Documents\\Projects.txt", "a");
+    if (fptr == NULL) {
+        printf("Error opening the file.\n");
+        exit(1);
+    }
+
+    
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter name of student %d: ", i);
+        scanf("%s", name);
+        printf("Enter marks of student %d: ", i);
+        scanf("%d", &marks);
+
+        fprintf(fptr, "Student %d: Name = %s, Marks = %d\n", i, name, marks);
+    }
+
+    fclose(fptr);
+    printf("Data written successfully to students.txt.\n");
+    return 0;
+}
