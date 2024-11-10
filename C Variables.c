ad3c6a1ac167db5698f758861087a0291544b32f
Mosesmatisi <mosesmatisi@gmail.com>
2024-11-10 21:33:45 +0300

    Add files via upload

---
+++ C Variables.c
@@ -0,0 +1,13 @@
+//C variables and Data types
+//Preprocessor directive
+#include<stdio.h> 
+int main(){
+    char a='M';
+    char name[]="MOSES";
+    int age=68;
+    float area=2.03;
+    double salary=2000.8;//lf;
+    printf("The character is %c \n",a);
+    printf("The string is %s \n",name);
+    return 0;
+}
