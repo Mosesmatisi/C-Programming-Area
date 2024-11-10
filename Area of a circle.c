97b35cd7c1a90e4d8ee30834e70c7b2bea5de42e
Mosesmatisi <mosesmatisi@gmail.com>
2024-11-10 14:08:59 +0300

    Add files via upload

--- Area of a circle.c
+++ Area of a circle.c
@@ -1,14 +1,22 @@
-//Area of a Cirle
+//Area of circle
 #include <stdio.h>
+
 #define PI 3.14159
+
+float calculateArea(float radius);
+
 int main() {
-	float radius, area;
-	
-	printf("Please Enter the Radius of the circle in cm: ");
-	scanf("%f", &radius);
-	
-	area = PI * radius * radius;
-	printf("The area of a circle in cm square is: %.2f\n", area);
-	
-	return 0;
+    float radius, area;
+
+    printf("Enter the radius in centimetres: ");
+    scanf("%f", &radius);
+
+    area = calculateArea(radius);
+    printf("The Area of the circle in centimetre square is: %.2f\n", area);
+
+    return 0;
+}
+
+float calculateArea(float radius) {
+    return PI * radius * radius;
 }
